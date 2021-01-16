98ab4f8faa65d0f6bccfecdc5f2e8c12a3b67449
solopeach <54593029+solopeach@users.noreply.github.com>
2021-01-16 15:35:35 -0500

    added searchbar

--- dashboard.py
+++ dashboard.py
@@ -1,13 +1,20 @@
-import tkinter import *
+from tkinter import *
 
 window = Tk()
+window.geometry('375x750')
 
 window.title("Welcome to WithU")
 
-def myClick():
-    
+searchBar = Entry(window, width = 20)
 
+searchBar.grid(column = 1, row = 0)
 
+def clicked():
+    searchInput = searchBar.get()
+
+searchBtn = Button(window, text = "Search", command=clicked)
+
+searchBtn.grid(column=2, row=0)
 
 
 window.mainloop()
