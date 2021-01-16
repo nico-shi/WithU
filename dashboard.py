eac9d7b29de1a74b062c6ba301d609740c310299
solopeach <54593029+solopeach@users.noreply.github.com>
2021-01-16 17:40:41 -0500

    explore

--- dashboard.py
+++ dashboard.py
@@ -1,20 +1,42 @@
 from tkinter import *
 
 window = Tk()
-window.geometry('375x750')
+window.geometry('375x700')
 
 window.title("Welcome to WithU")
+window.configure(background="#BEE7E8", bd=0)
+
+heading = Label(window, text = "Explore Page")
+heading.configure(background="#BEE7E8", bd=0)
+heading.place(x = 153, y=10)
 
 searchBar = Entry(window, width = 20)
 
-searchBar.grid(column = 1, row = 0)
+searchBar.place(x = 100, y=50)
 
 def clicked():
     searchInput = searchBar.get()
 
 searchBtn = Button(window, text = "Search", command=clicked)
 
-searchBtn.grid(column=2, row=0)
+searchBtn.place(x = 240, y=45)
 
+# Buttons
+photoLang = ""
+
+#language 
+languageBtn = Button(window, text="Language", bg="#A0D2DB", fg="#594157", font="Arial")
+languageBtn.place(x=110.5, y=200)
+languageBtn.config(height=3, width=13)
+
+#person/relation
+personBtn = Button(window, text="Relationship", bg="#A0D2DB", fg="#594157", font="Arial")
+personBtn.place(x=110.5, y=300)
+personBtn.config(height=3, width=13)
+
+#location
+locationBtn = Button(window, text="Location", bg="#A0D2DB", fg="#594157", font="Arial")
+locationBtn.place(x=110.5, y=400)
+locationBtn.config(height=3, width=13)
 
 window.mainloop()
