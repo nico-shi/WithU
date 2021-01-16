89bc61384d9b9b5f1537e61079964817161502a2
nico-shi <nicole2shi@gmail.com>
2021-01-16 15:25:03 -0500

    fix window size

--- gui_test.py
+++ gui_test.py
@@ -4,9 +4,9 @@ main_window = Tk()
 
 main_window.title("WithU")
 
-main_window.geometry('')
+main_window.geometry('375x812')
 
-welcome_label = Label(main_window, text="Welcome to WithU", font=("Arial Bold", 50))
+welcome_label = Label(main_window, text="Welcome to WithU", font=("Arial Bold", 35))
 
 welcome_label.grid(column=0, row=0)
 
