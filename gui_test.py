687abb5d51e0f39f9d8f141ce74914c2c4c64932
deanna-dc <deanna.dc.c@gmail.com>
2021-01-16 17:36:43 -0500

    Home Page GUI

--- gui_test.py
+++ gui_test.py
@@ -1,13 +1,29 @@
 from tkinter import *
+from PIL import ImageTk, Image
+from tkinter.ttk import Frame, Label, Style
 
-main_window = Tk()
+window = Tk()
+window.configure(background="#BEE7E8", bd=0)
 
-main_window.title("WithU")
+logo = Image.open("WithU_Logo.png")
+logo1 = ImageTk.PhotoImage(logo.resize((300, 300)))
+label = Label(image=logo1)
 
-main_window.geometry('375x812')
+label.image = logo1
+label.place(x=35, y=20)
 
-welcome_label = Label(main_window, text="Welcome to WithU", font=("Arial Bold", 35))
+#EXPLORE BUTTON
+exploreButton = Button(window, text="Explore/Search", bg="#A0D2DB", fg="#594157", font="Arial")
+exploreButton.grid(row=0, column=1)
+exploreButton.place(x=95.5, y=390) #Change Placement
+exploreButton.config(height=5, width=20)
 
-welcome_label.grid(column=0, row=0)
+#SAVED BUTTON
+savedButton = Button(window, text="Saved/Favourites", bg="#A0D2DB", fg="#594157", font="Arial")
+savedButton.grid(row=0, column=1)
+savedButton.place(x=95.5, y=540) #Change Placement
+savedButton.config(height=5, width=20)
+window.geometry("375x700")
+window.title("tinker test")
 
-main_window.mainloop()
+window.mainloop()
